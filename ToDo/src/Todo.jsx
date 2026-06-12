6101ae9b976e87b1dfe9001628bdf7e4aac13cab
Paranjoy <paranjoyhazarika@gmail.com>
2026-06-12 21:58:15 +0530

    Fixed some funcitonalities

--- ToDo/src/Todo.jsx
+++ ToDo/src/Todo.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import TrashIcon from './assets/trash-can-regular-full.svg'
+import PlusIcon from './assets/plus-solid-full.svg'
+
 import './Todo.css'
 
 function Todo() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem('tasks');
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   function addTask(e) {
     e.preventDefault();
-
     const input = e.target.elements.taskInput.value;
     if (input.trim() === "") return;
-    
-    setTasks([...tasks, input]);
+    setTasks([...tasks, { text: input, done: false }]);
     e.target.elements.taskInput.value = "";
+  }
+
+  function toggleTask(index) {
+    setTasks(tasks.map((task, i) =>
+      i === index ? { ...task, done: !task.done } : task
+    ));
   }
 
   function deleteTask(index) {
@@ -23,13 +37,23 @@ function Todo() {
       <h1 className="header">To-Do List</h1>
       <form onSubmit={addTask} className="todo-form">
         <input type="text" name="taskInput" placeholder="Enter your task..." />
-        <button type="submit">Add</button>
+        <button type="submit">
+          <img src={PlusIcon} alt="add-icon" id='plus-icon'/>
+          <span className='extra-text'>Add</span>
+        </button>
       </form>
 
     {tasks.map((task, i) => (
-      <div key={i} className="task">
-        <p>{i+1}. {task}</p>
-        <button onClick={() => deleteTask(i)}>Del</button>
+      <div key={i} className={`task ${task.done ? 'done' : ''}`}>
+        <p>
+          <span className='id-num'>{i + 1}</span>
+          <input type="checkbox" checked={task.done} onChange={() => toggleTask(i)} />
+          <span className='inp-task' title={task.text}>{task.text}</span>
+        </p>
+        <button onClick={() => deleteTask(i)}>
+          <img src={TrashIcon} alt='delete' id='trash-can'/>
+          <span className='extra-text'>Del</span>
+        </button>
       </div>
     ))}
     </div>
